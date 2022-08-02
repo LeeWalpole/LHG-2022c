12f233c3971d07c814f72e6d3af6e27b822d0520
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-08-02 15:31:57 +0700

    Update hero.php

--- hero/hero.php
+++ hero/hero.php
@@ -6,6 +6,52 @@ $hero_subheadline = get_field('hero_subheadline');
 
 
 
+
+
+
+<?php if (is_single() )  { ?>
+
+
+<?php // Headlines for Hero
+$hero_kicker = get_field('hero_kicker') ?: wp_get_post_terms( get_the_ID(), 'category' )[0]->name;
+$hero_headline = get_field('hero_headline') ?: get_the_title(); 
+$hero_subheadline = get_field('hero_subheadline'); 
+?>
+
+<div class="hero">
+    <section class="hero-stack">
+        <header class="header">
+            <strong class="kicker"><?php echo $hero_kicker; ?></strong>
+            <h3 class="headline"><?php echo $hero_headline; ?></h3>
+            <div class="subheadline"><?php echo $hero_subheadline; ?></div>
+            <?php /* if ($hero_cta_1) : ?>
+            <?php include( 'cta.php' ); // Call to Action Buttons if available  ?>
+            <?php endif; */ ?>
+        </header>
+    </section>
+</div>
+
+<?php } elseif (is_tax() || is_category() || is_tag() || is_post_type_archive() )  { ?>
+
+    <div class="hero">
+    <section class="hero-stack">
+        <header class="header">
+            <h1 class="headline"><?php echo $hero_headline; ?></h1>
+            <div class="subheadline"><?php echo $hero_subheadline; ?></div>
+            <?php /* if ($hero_cta_1) : ?>
+            <?php include( 'cta.php' ); // Call to Action Buttons if available  ?>
+            <?php endif; */ ?>
+        </header>
+    </section>
+</div>
+
+<?php } else {?>
+
+<?php // Headlines for Hero
+$hero_headline = get_field('hero_headline') ?: get_the_title(); 
+$hero_subheadline = get_field('hero_subheadline'); 
+?>
+
 <div class="hero">
     <section class="hero-stack">
         <header class="header">
@@ -19,3 +65,4 @@ $hero_subheadline = get_field('hero_subheadline');
     </section>
 </div>
 
+<?php } ?>
