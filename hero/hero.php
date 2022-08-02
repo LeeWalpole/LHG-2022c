0662c404fb9b5696409dcb43caf233771c55ecbb
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-08-02 15:19:20 +0700

    Update hero.php

--- hero/hero.php
+++ hero/hero.php
@@ -1,52 +1,17 @@
 <?php // Headlines for Hero
-echo $kicker = get_field('hero_kicker') ?: wp_get_post_terms( get_the_ID(), 'category' )[0]->name;
-echo $headline = get_field('hero_headline') ?: get_the_title(); 
-echo $subheadline = get_field('hero_subheadline'); 
+echo $hero_kicker = get_field('hero_kicker') ?: wp_get_post_terms( get_the_ID(), 'category' )[0]->name;
+echo $hero_headline = get_field('hero_headline') ?: get_the_title(); 
+echo $hero_subheadline = get_field('hero_subheadline'); 
 ?>
 
-
-
-<?php if (is_tax() || is_category() || is_tag() || is_post_type_archive() || is_front_page() )  { ?>
-
 <div class="hero">
     <section class="hero-stack">
         <header class="header">
-            <?php if ($headline) : ?><h1 class="headline"><?php echo $headline ?></h1><?php endif; ?>
-            <?php if ($subheadline) : ?><div class="subheadline"><?php echo $subheadline ?></div>
+            <?php if ($hero_headline) : ?><h1 class="headline"><?php echo $hero_headline ?></h1><?php endif; ?>
+            <?php if ($hero_subheadline) : ?><div class="subheadline"><?php echo $hero_subheadline ?></div>
             <?php /* if ($hero_cta_1) : ?>
             <?php include( 'cta.php' ); // Call to Action Buttons if available  ?>
             <?php endif; */ ?>
         </header>
     </section>
 </div>
-
-<?php } elseif (is_single() )  { ?>
-
-<div class="hero">
-    <section class="hero-stack">
-        <header class="header">
-            <?php if ($kicker) : ?><strong class="kicker"><?php echo $kicker ?></strong><?php endif; ?>
-            <?php if ($headline) : ?><h1 class="headline"><?php echo $headline ?></h1><?php endif; ?>
-            <?php if ($subheadline) : ?><div class="subheadline"><?php echo $subheadline ?></div>
-            <?php /* if ($hero_cta_1) : ?>
-            <?php include( 'cta.php' ); // Call to Action Buttons if available  ?>
-            <?php endif; */ ?>
-        </header>
-    </section>
-</div>
-
-<?php } else { ?>
-
-    <div class="hero">
-    <section class="hero-stack">
-        <header class="header">
-            <?php if ($headline) : ?><h1 class="headline"><?php echo $headline ?></h1><?php endif; ?>
-            <?php if ($subheadline) : ?><div class="subheadline"><?php echo $subheadline ?></div>
-            <?php /* if ($hero_cta_1) : ?>
-            <?php include( 'cta.php' ); // Call to Action Buttons if available  ?>
-            <?php endif; */ ?>
-        </header>
-    </section>
-</div>
-
-<?php } ?>
