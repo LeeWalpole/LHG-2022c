e695aeffd2a5f1705a2275c74f777b63039637e5
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-08-15 02:20:10 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -122,4 +122,4 @@ function remove_wp_block_library_css(){
     wp_dequeue_style( 'wc-block-style' ); // REMOVE WOOCOMMERCE BLOCK CSS
     wp_dequeue_style( 'global-styles' ); // REMOVE THEME.JSON
     }
-    add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
+    add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 ); 
