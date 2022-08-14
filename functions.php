24123e488a113bcbc6bfbc1ad3c391584d742cc5
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-08-15 02:35:21 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -138,4 +138,7 @@ function remove_global_styles_and_svg_filters() {
 add_action('init', 'remove_global_styles_and_svg_filters');
 // This snippet removes the Global Styles and SVG Filters that are mostly if not only used in Full Site Editing in WordPress 5.9.1+
 // Detailed discussion at: https://github.com/WordPress/gutenberg/issues/36834
-// WP default filters: https://github.com/WordPress/WordPress/blob/7d139785ea0cc4b1e9aef21a5632351d0d2ae053/wp-includes/default-filters.php
+// WP default filters: https://github.com/WordPress/WordPress/blob/7d139785ea0cc4b1e9aef21a5632351d0d2ae053/wp-includes/default-filters.ph
+
+remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
+remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
