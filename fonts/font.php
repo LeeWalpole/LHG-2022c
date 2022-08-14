76719f1fdc2445ba014ed669b578d38e97cdb994
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-08-15 02:23:54 +0700

    swap

--- fonts/font.php
+++ fonts/font.php
@@ -1,20 +1,20 @@
 <link rel="preload" as="font" href="<?php echo get_stylesheet_directory_uri(); ?>/fonts/circular-book.woff2"
 	type="font/woff2" crossorigin="anonymous">
-<link rel="preload" as="font" href="<?php echo get_stylesheet_directory_uri(); ?>/fonts/circular-black.woff2"
+<link rel="preload" as="font" href="<?php echo get_stylesheet_directory_uri(); ?>/fonts/circular-bold.woff2"
 	type="font/woff2" crossorigin="anonymous">
 
 <style>
 	@font-face {
 		font-family: "Circular-Book";
 		src: url("<?php echo get_stylesheet_directory_uri(); ?>/fonts/circular-book.woff2") format("woff2");
-		font-display: block;
+		font-display: swap;
 		
 	}
 
 	@font-face {
 		font-family: "Circular-Bold";
 		src: url("<?php echo get_stylesheet_directory_uri(); ?>/fonts/circular-bold.woff2") format("woff2");
-		font-display: block;
+		font-display: swap;
 	}
 
 	:root {
