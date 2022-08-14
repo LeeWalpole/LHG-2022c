b10cfbabf342c61c6799824df17bdb49d34a022c
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-08-15 02:19:54 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -114,3 +114,12 @@ function wps_deregister_styles() {
     wp_dequeue_style( 'global-styles' );
 }
 add_action( 'wp_enqueue_scripts', 'wps_deregister_styles', 100 );
+
+//REMOVE GUTENBERG BLOCK LIBRARY CSS FROM LOADING ON FRONTEND
+function remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-block-style' ); // REMOVE WOOCOMMERCE BLOCK CSS
+    wp_dequeue_style( 'global-styles' ); // REMOVE THEME.JSON
+    }
+    add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
