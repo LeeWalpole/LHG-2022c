7057fb3965aeab796a89d22c78ebfa281ead8448
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-08-15 03:09:31 +0700

    dm sans

--- fonts/font.php
+++ fonts/font.php
@@ -1,3 +1,4 @@
+<?php /*
 <link rel="preload" as="font" href="<?php echo get_stylesheet_directory_uri(); ?>/fonts/circular-book.woff2"
 	type="font/woff2" crossorigin="anonymous">
 <link rel="preload" as="font" href="<?php echo get_stylesheet_directory_uri(); ?>/fonts/circular-bold.woff2"
@@ -21,7 +22,6 @@
 		--font-1: 'Circular-Book', Helvetica, Arial, sans-serif;
 		--font-1b: 'Circular-Bold', Helvetica, Arial, sans-serif;
 	}
-
 	*, html, body, p, ul, li, a {
 		font-family: 'Circular-Book'; 
 	}
@@ -32,5 +32,29 @@
 	h4,
 	h5 {
 		font-family: 'Circular-Bold';
+	}
+	</style>
+
+	*/?>
+	
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+<style>
+
+	*, html, body, p, ul, li, a {
+		font-family: 'DM Sans', sans-serif;
+		font-weight:400;
+	}
+
+	h1,
+	h2,
+	h3,
+	h4,
+	h5 {
+		font-family: 'DM Sans', sans-serif;
+		font-weight:700;
 	}
 </style>
