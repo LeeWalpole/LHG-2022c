39f1b9204befad364250bb8c565b8a51d1926648
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-08-15 15:24:56 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php // Lads Holiday Guide 2022 functions and definitions
-add_action( 'after_setup_theme', 'lw_theme_support' );
 add_theme_support( 'title-tag' );
 add_theme_support( 'post-thumbnails' );    
 add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
