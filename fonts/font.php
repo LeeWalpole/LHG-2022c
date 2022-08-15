cf92aeb02f808ab9bf241972a00603676238755c
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-08-15 15:50:18 +0700

    functions

--- fonts/font.php
+++ fonts/font.php
@@ -2,7 +2,6 @@
 	type="font/woff2" crossorigin="anonymous">
 <link rel="preload" as="font" href="<?php echo get_stylesheet_directory_uri(); ?>/fonts/circular-bold.woff2"
 	type="font/woff2" crossorigin="anonymous">
-
 <style>
 	@font-face {
 		font-family: "Circular-Book";
@@ -10,7 +9,6 @@
 		font-display: swap;
 		
 	}
-
 	@font-face {
 		font-family: "Circular-Bold";
 		src: url("<?php echo get_stylesheet_directory_uri(); ?>/fonts/circular-bold.woff2") format("woff2");
@@ -22,7 +20,7 @@
 		--font-1b: 'Circular-Bold', Helvetica, Arial, sans-serif;
 	}
 	*, html, body, p, ul, li, a {
-		font-family: 'Circular-Book'; 
+		font-family: var(--font-1); 
 	}
 
 	h1,
@@ -30,7 +28,7 @@
 	h3,
 	h4,
 	h5 {
-		font-family: 'Circular-Bold';
+		font-family: var(--font-1b); 
 	}
 	</style>
 
--- functions.php
+++ functions.php
@@ -1,53 +1,70 @@
 <?php 
 if ( ! function_exists( 'myfirsttheme_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ */
+function myfirsttheme_setup() {
+ 
     /**
-     * Sets up theme defaults and registers support for various WordPress features.
-     *
-     * Note that this function is hooked into the after_setup_theme hook, which runs
-     * before the init hook. The init hook is too late for some features, such as indicating
-     * support post thumbnails.
+     * Make theme available for translation.
+     * Translations can be placed in the /languages/ directory.
      */
-    function myfirsttheme_setup() {
-        /*
-         * Make theme available for translation.
-         * Translations can be placed in the /languages/ directory.
-         */
-        load_theme_textdomain( 'myfirsttheme', get_template_directory() . '/languages' );
-        // Add default posts and comments RSS feed links to &lt;head>.=
-        add_theme_support( 'automatic-feed-links' );
-        // Enable support for post thumbnails and featured images.
-        add_theme_support( 'title-tag' );
-        add_theme_support( 'post-thumbnails' );     
-        add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
-        add_theme_support( 'post-formats', array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
-        // Resize Default Wordpress images
-        update_option( 'thumbnail_size_w', 360 );
-        update_option( 'thumbnail_size_h', 9999 );
-        update_option( 'thumbnail_crop', false );
-        update_option( 'medium_size_w', 640 );
-        update_option( 'medium_size_h', 9999 );
-        update_option( 'medium_crop', false );
-        update_option( 'medium_large_size_w', 640 );
-        update_option( 'medium_large_size_h', 9999 );
-        update_option( 'medium_large_crop', false );
-        update_option( 'large_size_w', 1920 );
-        update_option( 'large_size_h', 9999 );
-        update_option( 'large_crop', true );
-        update_option( '2x1_desktop_big', 1920, 960, true );
-        update_option( '2x1_desktop', 1366, 683, true );
-        update_option( '2x1_smartphone', 400, 200, true );
-        /**
-         * Add support for two custom navigation menus.
-         */
-        register_nav_menus( array(
-            'primary'   => __( 'Primary Menu', 'myfirsttheme' ),
-            'secondary' => __('Secondary Menu', 'myfirsttheme' )
-        ) );
-        /**
-         * Enable support for the following post formats:
-         * aside, gallery, quote, image, and video
-         */
-        // Remove Header things
+    load_theme_textdomain( 'myfirsttheme', get_template_directory() . '/languages' );
+ 
+    /**
+     * Add default posts and comments RSS feed links to &lt;head>.
+     */
+    add_theme_support( 'automatic-feed-links' );
+
+    /**
+     * Enable support for post thumbnails and featured images.
+     */
+    add_theme_support( 'post-thumbnails' );
+         // Enable support for post thumbnails and featured images.
+         add_theme_support( 'title-tag' ); 
+         add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
+         add_theme_support( 'post-formats', array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
+         // Resize Default Wordpress images
+         update_option( 'thumbnail_size_w', 360 );
+         update_option( 'thumbnail_size_h', 9999 );
+         update_option( 'thumbnail_crop', false );
+         update_option( 'medium_size_w', 640 );
+         update_option( 'medium_size_h', 9999 );
+         update_option( 'medium_crop', false );
+         update_option( 'medium_large_size_w', 640 );
+         update_option( 'medium_large_size_h', 9999 );
+         update_option( 'medium_large_crop', false );
+         update_option( 'large_size_w', 1920 );
+         update_option( 'large_size_h', 9999 );
+         update_option( 'large_crop', true );
+         update_option( '2x1_desktop_big', 1920, 960, true );
+         update_option( '2x1_desktop', 1366, 683, true );
+         update_option( '2x1_smartphone', 400, 200, true );
+    /**
+     * Add support for two custom navigation menus.
+     */
+    register_nav_menus( array(
+        'primary'   => __( 'Primary Menu', 'myfirsttheme' ),
+        'secondary' => __('Secondary Menu', 'myfirsttheme' )
+    ) );
+ 
+    /**
+     * Enable support for the following post formats:
+     * aside, gallery, quote, image, and video
+     */
+    add_theme_support( 'post-formats', array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
+}
+endif; // myfirsttheme_setup
+
+add_action( 'after_setup_theme', 'myfirsttheme_setup' );
+
+
+
+// Remove Header things
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
@@ -67,11 +84,3 @@ remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
 remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
 remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
-wp_dequeue_style( 'wp-block-library' );
-wp_dequeue_style( 'wp-block-library-theme' );
-wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
-add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
-    }
-    endif; // myfirsttheme_setup
-    add_action( 'after_setup_theme', 'myfirsttheme_setup' ); 
-?>
