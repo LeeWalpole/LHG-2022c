b1d17f13e8aa600ffd1c7d7922fa30f1333bb952
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-08-16 00:35:51 +0700

    Update page.php

--- page.php
+++ page.php
@@ -1,58 +1,85 @@
 <?php get_header(); // includes nav and hero ?>
 
-<style>
-.article-body img  { 
-width:100%!important;
-object-fit:contain!important;
-height:auto;
-} 
+<?php if( have_rows('hero_header') ):
+        while( have_rows('hero_header') ) : the_row();?>
 
-.article-body > p > a  { 
-color:#ee0099;
-} 
-</style>
+<?php 
+        $kicker = get_sub_field('kicker');
+        $headline = get_sub_field('headline');
+        $subheadline = get_sub_field('subheadline');
+        $hero_cta_1 = get_sub_field('cta_1');
+        $hero_cta_1_target = $hero_cta_1['target'] ? $hero_cta_1['target'] : '_self'; 
+        $hero_cta_1_title = $hero_cta_1['title'];
+        $hero_cta_1_url = $hero_cta_1['url'];
+        $hero_cta_2 = get_sub_field('cta_2');
+        $hero_cta_2_target = $hero_cta_2['target'] ? $hero_cta_2['target'] : '_self'; 
+        $hero_cta_2_title = $hero_cta_2['title'];
+        $hero_cta_2_url = $hero_cta_2['url'];
+        ?>
+
+<section class="hero-stack">
+    <header class="header w-max m-auto">
+        <?php if ($kicker) : ?><strong class="kicker"><?php echo $kicker ?></strong><?php endif; ?>
+        <?php if ($headline) : ?><h2 class="headline"><?php echo $headline ?></h2><?php endif; ?>
+        <?php if ($subheadline) : ?><div class="hero_subheadline"><?php echo $subheadline ?></div><?php endif; ?>
+        <?php if ($hero_cta_1) : ?>
+        <?php // include( 'cta.php' ); // Call to Action Buttons if available  ?>
+        <?php include( 'get_cta.php' ); ?>
+        <?php endif; ?>
+    </header>
+</section>
+
+<?php endwhile; // hero_header
+    endif; // hero_header ?>
 
 <?php if ( have_posts() ) :  while ( have_posts() ) : the_post();  ?>
 
-<?php get_template_part( 'snippets/snippet-hero' ); ?>
-<?php get_template_part( 'snippets/snippet-feature' ); ?>
+<?php // get_template_part( 'snippets/snippet-hero' ); ?>
+<?php // get_template_part( 'snippets/snippet-feature' ); ?>
+
 
 <?php if ( !empty( get_the_content() ) ) : ?>
 
-    <?php get_template_part( 'snippets/ad', 'header' ); ?>
+<div class="article-block">
 
-<section class="row-block bg-white grid grid-gap w-max">
-    <?php get_template_part( 'snippets/snippet', 'side-sharers' ); // col-1 ?>
-    <article class="article-body w-safe colspan-7">
-        <header class="header w-max">
-            <!-- <strong class="kicker">Kicker Category</strong> -->
-            <h1 class="title"><?php the_title(); ?></h1>
-            <?php if ( ! has_excerpt() ) { ?>
-            <?php } else {?>
-            <em class="description"><?php echo get_the_excerpt();?></em>
-            <?php } ?>
-        </header>
-        <?php // get_template_part( 'snippets/snippet', 'byline' ); ?>
- <!-- 
-        <aside id="chapters" class="bg-black">
-        </aside>
- -->
-        <?php the_content(); ?>
-        <?php get_template_part( 'snippets/snippet', 'article-sharers' ); // col-4 ?>
-    </article>
-    <?php get_template_part( 'snippets/ad', 'sidebar' ); ?>
-</section>
-<?php endif; ?>
-<?php endwhile; endif; ?>
+    <?php if ( get_the_post_thumbnail_url() ) : ?>
+    <figure class="feature colspan-12">
+        <img src="<?php echo get_the_post_thumbnail_url(); ?>" loading="eager">
+    </figure>
+    <?php endif; ?>
 
+    <section class="grid bg-white w-max m-auto">
+        <article class="article-body colspan-12 w-safe m-auto">
+            <header class="header w-max">
+                <!-- <strong class="kicker">Kicker Category</strong> -->
+                <h1 class="article-title"><?php the_title(); ?></h1>
+                <?php if ( ! has_excerpt() ) { ?>
+                <?php } else {?>
+                <em class="article-description"><?php echo get_the_excerpt();?></em>
+                <?php } ?>
+            </header>
 
-<p>Blocks Below</p>
+            <?php // get_template_part( 'snippets/byline' ); ?>
 
-<?php get_template_part( 'blocks/blocks' ); ?>
+            <?php // get_template_part( 'snippets/ad', 'header' ); ?>
 
-<p>Blocks Above</p>
+            <?php the_content(); ?>
 
-<?php // get_template_part( 'snippets/snippet', 'related' ); // col-4 ?>
+            <?php // get_template_part( 'snippets/snippet', 'article-sharers' ); // col-4 ?>
+        </article>
 
 
-<?php get_footer();  ?>
+
+
+    </section>
+    <?php endif; // !empty( get_the_content() ?>
+
+    	
+    <?php endwhile; endif; // loop ?>
+
+    <?php get_template_part( 'snippets/related' ); // col-4 ?>
+
+    <?php get_template_part( 'blocks/blocks' ); ?>
+
+
+    <?php get_footer();  ?>
