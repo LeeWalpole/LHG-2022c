b4831bad7f5939ca5e666698571c5cc2c0bddebc
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-08-15 17:55:48 +0700

    Update single.php

--- single.php
+++ single.php
@@ -87,8 +87,9 @@
     </section>
     <?php endif; // !empty( get_the_content() ?>
 
+    	
     <?php 
-    
+$post_id = get_the_ID();
 $load_scripts = get_field('load_scripts', $post_id);
 if( $load_scripts && in_array('awin', $load_scripts) ) {
 echo "yes";
