87a2610027a6fae572ffca3623d1ebccc376f086
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-09-03 18:53:32 +0700

    defer jquery

--- footer.php
+++ footer.php
@@ -278,7 +278,7 @@ echo "yes";
 </style>
 
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script defer src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script> -->
 <script>
     const scriptURL =
