0ba79a72081833106d87671285889b8460a4b704
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-09-07 17:12:15 +0700

    Update lw_block_puff.php

--- template-parts/blocks/lw_block_puff.php
+++ template-parts/blocks/lw_block_puff.php
@@ -31,10 +31,10 @@ if( have_rows('puff') ):
         // Do something...
         ?>
 
-<p>Image: <?php echo $puff_image_url; ?></p>
+<p>Image: <?php echo $puff_image_url[0]; ?></p>
 <aside id="puff_<?php echo $block_id; ?>">
     <a href="<?php echo $puff_link_url ?: "javascript:void(0)"; ?>" title="<?php echo esc_attr($puff_link_title); ?>" target="<?php echo esc_attr( $puff_link_target ); ?>">
-        <img src="<?php echo $puff_image_url; ?>" data-src="<?php echo $puff_image_url; ?>" alt="<?php echo $puff_headline; ?>" class="lazyload" loading="lazy">
+        <img src="<?php echo $puff_image_url[0]; ?>" data-src="<?php echo $puff_image_url[0]; ?>" alt="<?php echo $puff_headline; ?>" class="lazyload" loading="lazy">
         <?php if ($puff_headline) : ?><h5 class="headline"><?php echo $puff_headline; ?></h5><?php endif ?>
         <?php if ($puff_text) : ?><div class="puff_text"><?php echo $puff_text; ?></div><?php endif ?>
     </a>
